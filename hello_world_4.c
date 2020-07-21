522ddfcc86c7f518bcc60b80da7f629b7c794d63
miloskal <miloskal0108897@gmail.com>
2020-07-22 01:37:28 +0200

    Verzija cetiri hehe
    Izmenjena linija "return 0;" u
    liniju "exit(EXIT_SUCCESS);"

---
+++ hello_world_4.c
@@ -0,0 +1,22 @@
+// cetvrta verzija
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+static void
+error_fatal(char *msg)
+{
+	perror(msg);
+	exit(EXIT_FAILURE);
+}
+
+
+int
+main(int argc, char* argv[])
+{
+  printf("Hello World!\n");
+  
+  exit(EXIT_SUCCESS);
+}
