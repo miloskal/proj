c460621f39125419628733784d63c8697cbdd67e
miloskal <miloskal0108897@gmail.com>
2020-07-22 01:23:10 +0200

    This is description of commit 2 from editor

---
+++ hello_world_2.c
@@ -0,0 +1,22 @@
+// druga verzija
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+static void
+error_fatal(char *msg)
+{
+	perror(msg);
+	exit(EXIT_FAILURE);
+}
+
+
+int
+main()
+{
+  printf("Hello World!\n");
+  
+  return 0;
+}
